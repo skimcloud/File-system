abbd76b400dd749ee27b5f95685d318c5df1a962
skimcloud <113874029+skimcloud@users.noreply.github.com>
2023-03-16 18:16:17 -0700

    📁 Update fs.c (Phase 2 Done)

--- libfs/fs.c
+++ libfs/fs.c
@@ -9,7 +9,6 @@
 
 #define BLOCK_SIZE 4096
 #define FAT_EOC 0xFFFF
-#define MAX_FILES 128
 
 struct __attribute__((packed)) superBlock {
     char signature[8]; 			// Signature (must be equal to “ECS150FS”)
@@ -33,7 +32,7 @@ struct __attribute__((packed)) rootEntry {
 };
 
 struct __attribute__((packed)) rootDirectory {
-	struct rootEntry rootEntry[MAX_FILES];
+	struct rootEntry rootEntry[FS_FILE_MAX_COUNT];
 };
 
 
@@ -91,7 +90,7 @@ int fs_umount(void)
 	/* TODO: Phase 1 */
 	if (block_write(0, &super) == -1) {							// Superblock can't be read
 		return -1;
-	} else if (block_write(super_rootIndex, &root) == -1) {		// Root directory can't be written to
+	} else if (block_write(super.rootIndex, &root) == -1) {		// Root directory can't be written to
 		return -1;
 	}
 
@@ -125,17 +124,87 @@ int fs_info(void)
 int fs_create(const char *filename)
 {
 	/* TODO: Phase 2 */
+	if (filename == NULL || strlen(filename) > FS_FILENAME_LEN) {
+		return -1;
+	}
 
+
+	/* Traverse root directory before creation (Check if file exists and if max file count not exceeded) */
+	int fileCount = 0;
+	for(int i = 0; i < FS_FILE_MAX_COUNT; i++) {
+		if (strcmp((char*)rootdir.entry[i].filename, filename) == 0) {
+			return -1; 
+		} 
+		if (rootdir.entry[i].filename[0] != '\0') { // Non empty entry found, increment file count
+			fileCount++;										
+		}
+	}
+
+	/* Max File Count Exceeded */
+	if (fileCount >= FS_FILE_MAX_COUNT) {
+		return -1;
+	}
+
+	/* Create File */
+	for (int i = 0; i < FS_FILE_MAX_COUNT; i++) {
+		if (rootdir.entry[i].filename[0] == '\0') {								// Empty entry found
+			memcpy(root.rootEntry[i].fileName, filename, FS_FILENAME_LEN); 		// Copy file name
+			root.rootEntry[i].fileSize = 0; 									// Set root dir size to 0
+			root.rootEntry[i].dataBlockIndex = FAT_EOC;  							// first data block starts from 0xFFFF
+			block_write(super.rootIndex, &root);
+			break;
+		}
+	}
+
+	return 0;
 }
 
 int fs_delete(const char *filename)
 {
 	/* TODO: Phase 2 */
+	if (filename == NULL) {
+		return -1;
+	}
+
+	uint16_t index = FAT_EOC;
+	bool fileFound = false;
+	for (int i = 0; i < FS_FILE_MAX_COUNT; i++) {
+		if (strcmp((char*)root.rootEntry[i].fileName, filename) == 0) {
+			fileFound = true;
+			index = root.rootEntry[i].dataIndex;
+
+			/* Destroy Root Entry */
+			root.rootEntry[i].fileName[0] = '\0';
+			root.rootEntry[i].fileSize = 0;
+			root.rootEntry[i].dataBlockIndex = FAT_EOC;
+			block_write(super.rootIndex, &root);
+			break;
+		}
+	}
+
+	if (!fileFound) {
+		return -1;
+	}
+
+	while (index != FAT_EOC) {
+		uint16_t next = fat.arr[index];
+		fat.arr[index] = 0;
+		index = next;
+	}
+
+	return 0;
 }
 
 int fs_ls(void)
 {
-	/* TODO: Phase 2 */
+	printf("FS Ls:\n");
+	for (int i = 0; i < FS_FILE_MAX_COUNT; i++) {
+		if (root.rootEntry[i].fileName[0] != '\0') {
+			struct rootEntry entry = root.rootEntry[i];
+			printf("File Name: %s, Size: %i, Data Block Index: %i\n", (char*)entry.fileName, entry.fileSize, entry.dataBlockIndex);
+		}
+	}
+	return 0;
 }
 
 int fs_open(const char *filename)
